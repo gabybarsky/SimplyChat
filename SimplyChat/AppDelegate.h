7d48360bae2112d17c2474ef160b4a8fbaf48716
Gabriel Barsky <gaby@liveathos.com>
2014-09-20 00:02:24 -0400

    Initial Commit

---
+++ SimplyChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimplyChat
+//
+//  Created by Gabriel Barsky on 2014-09-20.
+//  Copyright (c) 2014 Gabriel Barsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimplyChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimplyChat
+//
+//  Created by Gabriel Barsky on 2014-09-20.
+//  Copyright (c) 2014 Gabriel Barsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
